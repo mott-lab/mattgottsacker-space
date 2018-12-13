5a9d358ea362876fb9c7faa61d5be5a5e6f76b35
Matt Gottsacker <mgottsacker34@gmail.com>
2018-12-13 13:49:06 -0600

    Styled the selection arrows  like hyperlinks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,43 @@
+// Replace all img tags with svg elements with inline svg elements. This allows
+// CSS to manipulate the colors of the svg elements.
+jQuery('img.svg').each(function(){
+  var $img = jQuery(this);
+  var imgID = $img.attr('id');
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+
+  jQuery.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if(typeof imgID !== 'undefined') {
+          $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if(typeof imgClass !== 'undefined') {
+          $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+      if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+  }, 'xml');
+
+});
+
+(function($){
+
+})(jQuery);
+
 //function to toggle the visibility of the underscores
 (function($){
   $.fn.visible = function() {
@@ -17,6 +57,19 @@
   var toggle_visibility = setInterval(function(){
     $("#underscore").visibilityToggle();
   }, 500);
+
+  // When hovering over whole svg, highlight svg path
+  $(function() {
+    $('svg').hover(
+      function(){
+        $(this).children().first().addClass('chevronSelected');
+      },
+      function(){
+        $(this).children().first().removeClass('chevronSelected');
+      }
+    );
+  });
+
 })(jQuery);
 
 $(".nav .nav-link").on("click", function(){
