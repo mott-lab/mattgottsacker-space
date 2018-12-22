a3c9436ef7d5ba80cd06fc0a6526bef14a3b8f0e
Matt Gottsacker <mgottsacker34@gmail.com>
2018-12-21 22:52:31 -0600

    minor config edits

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mg-space' });
+  res.render('index', { title: 'matt gottsacker' });
 });
 
 module.exports = router;
