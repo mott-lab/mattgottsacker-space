c13815d7058939e0a589fef229464241fcee1418
Matt Gottsacker <mgottsacker34@gmail.com>
2019-08-19 20:17:48 -0500

    redo submodules

--- routes/comp-prog.js
+++ routes/comp-prog.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET bio page. */
 router.get('/muhs-comp-prog', function(req, res, next) {
-  res.redirect('../lib/comp-prog-website/');
+    console.log(req);
+  res.sendFile('lib/comp-prog-website/');
 });
 
 module.exports = router;
