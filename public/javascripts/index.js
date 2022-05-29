b5cbdee75caa96e6a515e238ba2702ea945904b2
Matt Gottsacker <mgottsacker34@gmail.com>
2022-05-29 14:50:37 -0400

    compressed profile pics and added autoswitch

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,22 @@
 // Change profile picture when user hovers over it.
-var numHovers = 0;
-$('.profile-pic').on("mouseenter click", function(){
-  var hoverPic = 'images/profile-pic-b-' + (numHovers % 6) + '.jpg'
-  $(this).attr('src', hoverPic);
-  numHovers++;
-});
+// var numHovers = 0;
+// $('.profile-pic').on("mouseenter click", function(){
+//   var hoverPic = 'images/profile-pic-b-' + (numHovers % 6) + '-1200.jpg'
+//   $(this).attr('src', hoverPic);
+//   numHovers++;
+// });
 
 var map = L.map('map').setView([0,0], 1);
 L.tileLayer.provider('Esri.WorldImagery').addTo(map);
 var marker = L.marker([28.538336, -81.379234]).addTo(map);
+
+var currentPic = 'images/profile-pic-b-0-1200.jpg';
+var picIdx = 0;
+function nextPic() {
+  picIdx++;
+  picIdx %= 6;
+  picSrc = 'images/profile-pic-b-' + picIdx + '-1200.jpg';
+  $('.profile-pic').attr('src', picSrc);
+}
+
+var picSwitch = setInterval(nextPic, 2000);
