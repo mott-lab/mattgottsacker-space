786d16cdf72bd2e7d0a272496a6e73abd4f4eac2
Matt Gottsacker <mgottsacker34@gmail.com>
2024-12-03 07:53:49 -0500

    minor updates

--- public/mothers-day-2021/name-helper.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,22):
-  print('<img id="pic' + str(i) + '" src="./assets/mom-idxd/pic' + str(i) + '.jpg"></img>')
-for i in range(1,10):  
-  print('<video id="vid' + str(i) + '" src="./assets/mom-idxd/vid' + str(i) + '.mp4" autoplay loop="true"></video>')
